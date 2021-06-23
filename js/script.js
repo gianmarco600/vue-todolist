f6e6289f438d7b83df6f06b0683663b3912c6f02
gianmarco <gianmarco.palombo600@gmail.com>
2021-06-23 16:14:19 +0200

    fine

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Descrizione: Rifare l'esercizio della to do list come fatto assieme in classe:
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
+const app = new Vue({
+    el:'#app',
+    data:{
+        list:[
+            'Guardare tutti i film di Nanni',
+            'Fare il doumentario sull\'Italia, e sulla sua situazione politica',
+            'Girare un film su un pasticcere Trotzkista'
+        ],
+        input:''
+    },
+    methods:{
+        getInput(){
+            if (this.input != ''){
+                this.list.push(this.input);
+            }
+            this.input = '';
+        },
+        remove(indice){
+            this.list.splice(indice, 1,);
+        }
+    }
+
+})
